7446952d1fe385d287cbdcfd6ac14859c343f71d
Andre Ferreira <123772724+Andre2828Ferreira88@users.noreply.github.com>
2024-11-13 16:06:24 -0300

    Add files via upload

---
+++ JSPAINELDECONTROLE/paineldeControle.js
@@ -0,0 +1,65 @@
+// Espera até que o DOM esteja totalmente carregado
+document.addEventListener("DOMContentLoaded", function() {
+    // Pegue o elemento com o ID 'sair'
+    const btnSair = document.getElementById('SAIR');
+    
+    // Adiciona o evento de clique no botão 'Sair'
+    btnSair.addEventListener('click', function() {
+        // Redireciona para outra página
+        // Aqui, você pode colocar o link que deseja. Exemplo:
+        window.location.href = "./login.html";  // Redireciona para a página de logout
+        
+        // Caso queira abrir em uma nova aba:
+        // window.open('logout.html', '_blank');
+    });
+});
+
+
+// Espera até que o DOM esteja totalmente carregado
+document.addEventListener("DOMContentLoaded", function() {
+    // Pegue o elemento com o ID 'sair'
+    const btnSair = document.getElementById('FUNCIONARIOS');
+    
+    // Adiciona o evento de clique no botão 'Sair'
+    btnSair.addEventListener('click', function() {
+        // Redireciona para outra página
+        // Aqui, você pode colocar o link que deseja. Exemplo:
+        window.location.href = "./funcionarios.html";  // Redireciona para a página de logout
+        
+        // Caso queira abrir em uma nova aba:
+        // window.open('logout.html', '_blank');
+    });
+});
+
+// Espera até que o DOM esteja totalmente carregado
+document.addEventListener("DOMContentLoaded", function() {
+    // Pegue o elemento com o ID 'sair'
+    const btnSair = document.getElementById('VALORES');
+    
+    // Adiciona o evento de clique no botão 'Sair'
+    btnSair.addEventListener('click', function() {
+        // Redireciona para outra página
+        // Aqui, você pode colocar o link que deseja. Exemplo:
+        window.location.href = "./estoque.html";  // Redireciona para a página de logout
+        
+        // Caso queira abrir em uma nova aba:
+        // window.open('logout.html', '_blank');
+    });
+});
+
+
+// Espera até que o DOM esteja totalmente carregado
+document.addEventListener("DOMContentLoaded", function() {
+    // Pegue o elemento com o ID 'sair'
+    const btnSair = document.getElementById('GERAIS');
+    
+    // Adiciona o evento de clique no botão 'Sair'
+    btnSair.addEventListener('click', function() {
+        // Redireciona para outra página
+        // Aqui, você pode colocar o link que deseja. Exemplo:
+        window.location.href = "./telainicial.html";  // Redireciona para a página de logout
+        
+        // Caso queira abrir em uma nova aba:
+        // window.open('logout.html', '_blank');
+    });
+});
